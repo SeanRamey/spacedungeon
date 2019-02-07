8e11dd0ef4fe696b887995542fbfbb6485d1bf97
Sean Ramey <sramey40@gmail.com>
2019-02-07 15:20:28 -0600

    Cleaned up level loading code a bit

--- src/level.cpp
+++ src/level.cpp
@@ -70,7 +70,6 @@ void handleCollisions() {
 
 void Level::loadMap(std::string map, std::string images) {
 
-    std::vector<char> fileData;
     // Open the tile map data file for the level
     std::ifstream file(map); 
 
@@ -84,60 +83,68 @@ void Level::loadMap(std::string map, std::string images) {
     // for the dimensions of the map
     file >> mapSize.x >> mapSize.y;
 
+    // Load the rest of the file, and store each byte in fileData
+    std::vector<char> fileData;
     fileData.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
     file.close();
 
+    // Parse the fileData into an array of bytes, seperated by spaces and newlines
+    // for loop starts at 1 to skip the first newline character in the fileData
     std::string buffer;
-    std::vector<std::string> finalData;
-    for(int i = 1; i < fileData.size(); i++){
-        switch(fileData[i]){
+    std::vector<unsigned char> mapData;
+    for(int i = 1; i < fileData.size(); i++) {
+        switch(fileData[i]) {
             case ' ':
-                // add to final data the proper number read from the file
-                finalData.push_back(buffer); 
+                mapData.push_back((unsigned char)std::stoul(buffer)); // add the current buffer to mapData as an unsigned char
                 buffer.clear();
                 break;
             case '\n':
-                finalData.push_back(buffer);
+                mapData.push_back((unsigned char)std::stoul(buffer));
                 buffer.clear();
                 break;
             default:
-                buffer.push_back(fileData[i]);
+                buffer += fileData[i];
         }
     }
-    // image loading and setting up
-    sf::Vector2u tileCount;
-    sf::Image tileImage;
-    if(!tileImage.loadFromFile(images)){
+
+    // Load the tileset source image
+    sf::Image tilesetImage;
+    if(!tilesetImage.loadFromFile(images)){
        Log::error("big boy error, couldn't load tile map");
     }
 
-    tileCount.x = tileImage.getSize().x / tileSize; 
-    tileCount.y = tileImage.getSize().y / tileSize; 
+    // Store how many tiles are in the tileset
+    sf::Vector2u tileCount;
+    tileCount.x = tilesetImage.getSize().x / tileSize; 
+    tileCount.y = tilesetImage.getSize().y / tileSize; 
 
+    // Store each tile graphic as a sf::Texture in an array
     for(unsigned int y = 0; y < tileCount.y; y++) {
         for(unsigned int x = 0; x < tileCount.x; x++) {
             tileImages.push_back(new sf::Texture());
-            tileImages.at(y * tileCount.x + x)->loadFromImage(tileImage, sf::IntRect(x * tileSize, y * tileSize, tileSize, tileSize));
+            tileImages.at(y * tileCount.x + x)->loadFromImage(tilesetImage, sf::IntRect(x * tileSize, y * tileSize, tileSize, tileSize));
         }
     }
-
-    sf::Image backGroundImage;
-    backGroundImage.create(mapSize.x * tileSize, mapSize.y * tileSize, sf::Color::Black);
 
     // Create Tiles in the map
     for(unsigned int y = 0; y < mapSize.y; y++) {
         for(unsigned int x = 0; x < mapSize.x; x++) {
-            if(std::stoi(finalData.at(x + y * mapSize.x)) != std::stoi("0")) {
+            if(mapData.at(x + y * mapSize.x) != 0) {
                 Tile *tile = new Tile(x * tileSize, y * tileSize, tileSize, tileSize);
                 tiles.push_back(tile);
-                tiles[tiles.size() - 1]->setImage(tileImages[std::stoi(finalData[x + y * mapSize.x]) - 1]);
+                tiles[tiles.size() - 1]->setImage(tileImages[mapData[x + y * mapSize.x] - 1]);
             }
         }
     }
-    // loading backGround
+
+    // Create an image of the entire map so that only 1 draw call is needed for the whole map
+    sf::Image backGroundImage;
+    backGroundImage.create(mapSize.x * tileSize, mapSize.y * tileSize, sf::Color::Black);
+
     for(Tile* tile : tiles){
         backGroundImage.copy(tile->getTexture()->copyToImage(), tile->getPosition().x, tile->getPosition().y);
     }
+
     backGround.loadFromImage(backGroundImage); 
     backGroundSprite.setTexture(backGround);
 }
