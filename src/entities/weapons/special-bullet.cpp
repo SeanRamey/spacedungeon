6b77c82b29240a0e2fcd3ead18a4c70fc3842930
Minichota <minichota1219@github.com>
2019-04-06 16:49:43 -0500

    changed shrapnel

--- src/entities/weapons/special-bullet.cpp
+++ src/entities/weapons/special-bullet.cpp
@@ -8,7 +8,7 @@
 
 SpecialBullet::SpecialBullet(sf::Vector2f position, sf::Vector2f targetPosition, Entity* owner, Level* level) :
 Bullet(position, targetPosition, owner, sf::Vector2u(32,32), 200, 2, level, sf::seconds(1)){
-    numSplits = 8;
+    numSplits = 16;
     accelerate(sf::Vector2f(this->initialSpeed * cosf(degreesToRadians(getRotation() - 90)), this->initialSpeed * sinf(degreesToRadians(getRotation() - 90))));
 }
 
