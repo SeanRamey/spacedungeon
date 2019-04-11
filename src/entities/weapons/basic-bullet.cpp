51c583d91841a7bf0826c0c97ea6bc74bf364159
Sean Ramey <sramey40@gmail.com>
2019-04-10 20:04:28 -0500

    slow basic bullet down correct basic bullet sprite size
    Slowed the basic bullet down from 1000 to 200, also corrected the sprite size for the basic bullet.

--- src/entities/weapons/basic-bullet.cpp
+++ src/entities/weapons/basic-bullet.cpp
@@ -9,7 +9,7 @@
 using namespace Input;
 
 BasicBullet::BasicBullet(sf::Vector2f position, sf::Vector2f targetPosition, Entity* owner, Level* level):
-Bullet(position, targetPosition, owner, sf::Vector2u(8,16), 1000, 1, level){
+Bullet(position, targetPosition, owner, sf::Vector2u(12,16), 200, 1, level){
     offset = Util::GetRandomNumber(0, 8) - 4;
     accelerate(sf::Vector2f(initialSpeed * cosf(degreesToRadians(getRotation() - 90 + offset)), initialSpeed * sinf(degreesToRadians(getRotation() - 90 + offset))));
 }
