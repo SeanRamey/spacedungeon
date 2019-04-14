f0c6541bc2d0cf76893b169384dbfe9a28259d55
Minichota <minichota1219@github.com>
2019-04-13 23:03:00 -0500

    Added button ui item

---
+++ src/UI/UI-button.hpp
@@ -0,0 +1,19 @@
+#ifndef UI_BUTTON_HPP
+#define UI_BUTTON_HPP
+#include "UI-element.hpp"
+
+class UIButton : public UIElement {
+    private:
+        sf::Text text;
+        sf::Font font;
+
+    public:
+        UIButton(sf::Vector2f position, sf::Texture* texture);
+        ~UIButton();
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        void update(); // setting text to center of sprite
+        void setSize(sf::Vector2u size);
+        void setProperties(unsigned int fontSize = 35, sf::Color color = sf::Color::White, sf::Font* font = nullptr, sf::Vector2f position = sf::Vector2f(0, 0));
+
+};
+#endif
