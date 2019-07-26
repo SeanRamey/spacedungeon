c017e8d877b1381044c8c5b1909cee217d858c1d
Sean Ramey <sramey40@gmail.com>
2019-07-25 20:05:44 -0500

    progress on floatline collisions

--- src/unit-tests/floatline-tests.cpp
+++ src/unit-tests/floatline-tests.cpp
@@ -1,21 +1,49 @@
 #include "sfmlpch.hpp"
 #include <doctest/doctest.h>
 #include "floatline.hpp"
+#include <cmath>
+#include <iostream>
 
 TEST_CASE( "FloatLine perpendicular intersection test") {
-    FloatLine floatline1(sf::Vector2f(0,15), sf::Vector2f(20,15));
-    FloatLine floatline2(sf::Vector2f(10,25), sf::Vector2f(10,5));
     sf::Vector2f pointOfCollision(0,0);
 
-    CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
-    CHECK(pointOfCollision.x == 10);
-    CHECK(pointOfCollision.y == 15);
+    SUBCASE( "basic perpendicular intersection") {
+        FloatLine floatline1(sf::Vector2f(2,0), sf::Vector2f(2,4));
+        FloatLine floatline2(sf::Vector2f(0,2), sf::Vector2f(4,2));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 2);
+        CHECK(pointOfCollision.y == 2);
+    }
+
+    SUBCASE( "fast perpendicular intersection") {
+        FloatLine floatline1(sf::Vector2f(75,0), sf::Vector2f(75,150));
+        FloatLine floatline2(sf::Vector2f(0,75), sf::Vector2f(150,75));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 75);
+        CHECK(pointOfCollision.y == 75);
+    }
+
+    SUBCASE( "basic perpendicular intersection (endpoint)") {
+        FloatLine floatline1(sf::Vector2f(4,0), sf::Vector2f(4,4));
+        FloatLine floatline2(sf::Vector2f(0,2), sf::Vector2f(4,2));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 4);
+        CHECK(pointOfCollision.y == 2);
+    }
+
+    SUBCASE( "diagonal perpendicular intersection") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(4,4));
+        FloatLine floatline2(sf::Vector2f(4,0), sf::Vector2f(0,4));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 2);
+        CHECK(pointOfCollision.y == 2);
+	}
 }
 
 TEST_CASE( "FloatLine parrallel intersection test") {
     sf::Vector2f pointOfCollision(0,0);
 
-    SUBCASE( "horizontal parrallel intersection") {
+    SUBCASE( "horizontal indentical parrallel intersection") {
         FloatLine floatline1(sf::Vector2f(0,15), sf::Vector2f(20,15));
         FloatLine floatline2(sf::Vector2f(0,15), sf::Vector2f(20,15));
         CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
@@ -23,7 +51,7 @@ TEST_CASE( "FloatLine parrallel intersection test") {
         CHECK(pointOfCollision.y == 15);
     }
 
-    SUBCASE( "vertical parrallel intersection") {
+    SUBCASE( "vertical identical parrallel intersection") {
         FloatLine floatline1(sf::Vector2f(10,5), sf::Vector2f(10,25));
         FloatLine floatline2(sf::Vector2f(10,5), sf::Vector2f(10,25));
         CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
@@ -44,16 +72,56 @@ TEST_CASE( "FloatLine parrallel intersection test") {
     }
 
 	SUBCASE( "horizontal parrallel non-identical intersection") {
-        FloatLine floatline1(sf::Vector2f(0,15), sf::Vector2f(20,15));
-        FloatLine floatline2(sf::Vector2f(15,15), sf::Vector2f(20,15));
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(30,0));
+        FloatLine floatline2(sf::Vector2f(15,0), sf::Vector2f(30,0));
         CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
-        CHECK(pointOfCollision.x == 20);
-        CHECK(pointOfCollision.y == 15);
+        CHECK(pointOfCollision.x == 30);
+        CHECK(pointOfCollision.y == 0);
+	}
+
+    SUBCASE( "horizontal parrallel non-identical intersection 2") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(5,0));
+        FloatLine floatline2(sf::Vector2f(1,0), sf::Vector2f(3,0));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(std::roundf(pointOfCollision.x) == 2);
+        CHECK(pointOfCollision.y == 0);
+	}
+
+    SUBCASE( "horizontal parallel non-identical intersection (backwards)") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(5,0));
+        FloatLine floatline2(sf::Vector2f(3,0), sf::Vector2f(1,0));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(std::roundf(pointOfCollision.x) == 2);
+        CHECK(pointOfCollision.y == 0);
+	}
+
+    SUBCASE( "vertical parrallel non-identical intersection") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(0,30));
+        FloatLine floatline2(sf::Vector2f(0,15), sf::Vector2f(0,30));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 0);
+        CHECK(pointOfCollision.y == 30);
+	}
+
+    SUBCASE( "diagonal parallel intersection") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(4,4));
+        FloatLine floatline2(sf::Vector2f(0,0), sf::Vector2f(4,4));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 0);
+        CHECK(pointOfCollision.y == 0);
+	}
+
+    SUBCASE( "diagonal parallel non-identical intersection") {
+        FloatLine floatline1(sf::Vector2f(0,0), sf::Vector2f(4,4));
+        FloatLine floatline2(sf::Vector2f(2,2), sf::Vector2f(4,4));
+        CHECK(floatline1.get_line_intersection(floatline2.x1, floatline2.y1, floatline2.x2, floatline2.y2, &pointOfCollision) == true);
+        CHECK(pointOfCollision.x == 4);
+        CHECK(pointOfCollision.y == 4);
 	}
 
 }
 
-TEST_CASE( "FloatLine 45 degree intersection test") {
+TEST_CASE( "FloatLine diagonal intersection test") {
     FloatLine floatline1(sf::Vector2f(0,25), sf::Vector2f(20,5));
     FloatLine floatline2(sf::Vector2f(20,25), sf::Vector2f(0,5));
     sf::Vector2f pointOfCollision(0,0);
--- src/util/floatline.cpp
+++ src/util/floatline.cpp
@@ -129,37 +129,115 @@ bool FloatLine::intersects(const FloatLine& otherLine, sf::Vector2f* pointOfColl
 
 }
 
-// Returns 1 if the lines intersect, otherwise 0. In addition, if the lines 
-// intersect the intersection point may be stored in the floats pointOfCollision.x and pointOfCollision.y.
 bool FloatLine::get_line_intersection(float x1, float y1, float x2, float y2, sf::Vector2f* pointOfCollision)
 {
-    float width_1 = this->x2 - this->x1; // WIDTH  OF LINE
-    float height_1 = this->y2 - this->y1; // HEIGHT OF LINE
-    float width_2 = x2 - x1;             // WIDTH  OF LINE2
-    float height_2 = y2 - y1;             // HEIGHT OF LINE2
 
-	if(width_1 == 0) width_1 = FLT_MIN;
-	if(height_1 == 0) height_1 = FLT_MIN;
-	if(width_2 == 0) width_2 = -FLT_MIN;
-	if(height_2 == 0) height_2 = -FLT_MIN;
-
-    float s = (-height_1 * (this->x1 - x1) + width_1 * (this->y1 - y1)) / (-width_2 * height_1 + width_1 * height_2);
-    float t = ( width_2 * (this->y1 - y1) - height_2 * (this->x1 - x1)) / (-width_2 * height_1 + width_1 * height_2);
-
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
-    {
-        // Collision detected
-        pointOfCollision->x = this->x1 + (t * width_1);
-        pointOfCollision->y = this->y1 + (t * height_1);
+    // start point collision
+    if(this->x1 == x1 && this->y1 == y1) {
+        std::cout << "Lines start at same point!\n";
+        pointOfCollision->x = x1;
+        pointOfCollision->y = y1;
         return true;
     }
 
-	// last point collision endcase
-	if(this->y2 == y2 && this->x2 == x2){
-		pointOfCollision->x = x2;
-		pointOfCollision->y = y2;
-		return true;
-	}
+    // last point collision endcase
+    if(this->y2 == y2 && this->x2 == x2) {
+        std::cout << "Lines end at same point!\n";
+        pointOfCollision->x = x2;
+        pointOfCollision->y = y2;
+        return true;
+    }
+
+    float width1 = this->x2 - this->x1; // width of line1
+    float height1 = this->y2 - this->y1; // height of line1
+    float width2 = x2 - x1;             // width of line2
+    float height2 = y2 - y1;             // height of line2
+    float width1_1 = this->x1 - x1;     // width between the first points of both lines
+    float height1_1 = this->y1 - y1;    // height between the first points of both lines
+
+    float denominator = (-width2 * height1 + width1 * height2);
+
+    // handle parallel lines
+    if(denominator == 0) {
+
+        float slope1 = (width1) / (height1);
+        float slope2 = (width2) / (height2);
+        float slope1_1 = (x1 - this->x1) / (y1 - this->y1); // slope of the first point on the first line to the first point on the second line
+
+        float length1 = this->length(); //sqrtf(pow(width1, 2) + pow(height1, 2));
+        float length2 = sqrtf(pow(width2, 2) + pow(height2, 2));
+
+        // hack to avoid division by zero
+        // if(height1 == 0 || width1 == 0) slope1 = 0;
+        // if(height2 == 0 || width2 == 0) slope2 = 0;
+        // if(height1_1 == 0 || width1_1 == 0) slope1_1 = 0;
+
+        std::cout << "slope1=" << slope1 << "\n";
+        std::cout << "slope1_1=" << slope1_1 << "\n";
+        std::cout << "slope2=" << slope2 << "\n";
+
+        // test if lines are collinear
+        if(slope1 == slope2 && slope1_1 == slope1) {
+            std::cout << "Lines are collinear!\n";
+
+            // float time = (difference in starting points)/(len_big - len_lil)
+            // if (time > 0 || time < 1){
+            //     // Collision detected
+            //     pointOfCollision->x = this->x1 + (time * width1);
+            //     pointOfCollision->y = this->y1 + (time * height1);
+            //     return true;
+            // } else {
+            //     no collision
+            //     std::cout << "Lines are parallel and co-linear, but don't intersect!\n";
+            //     return false;
+            // }
+            
+            float swpLength;
+            if(length1 < length2) {
+                swpLength = length2;
+                length2 = length1;
+                length1 = swpLength;
+                std::cout << "lengths swapped\n";
+            }
+
+            std::cout << "length1=" << length1 << "\n";
+            std::cout << "length2=" << length2 << "\n";
+
+            float diff = x1 - this->x1;
+            float time = (diff)/(length1 - length2);
+            if (time > 0 || time < 1){
+                // Collision detected
+                std::cout << "time=" << time << "\n";
+                pointOfCollision->x = this->x1 + (time * width1);
+                pointOfCollision->y = this->y1 + (time * height1);
+                return true;
+            } else {
+                //no collision
+                std::cout << "Lines are parallel and co-linear, but don't intersect!\n";
+                return false;
+            }
+
+	    }
+
+        std::cout << "Lines are parallel and not co-linear!\n";
+        return false; // parallel lines that aren't co-linear will never intersect.
+    }
+
+    // s represents the percent of line1 from the start before the intersection.
+    // t represents the percent of line2 from the start before the intersection.
+    // for example, a line1 that is 10 units long with s = 0.5 will instersect with
+    // line2 at 5 units from the starting point. Same applies to line2 and t.
+    // s and t can be thought of in terms of time, as percent of total frame time.
+    float s = (-height1 * (width1_1) + width1 * (height1_1)) / denominator;
+    float t = ( width2 * (height1_1) - height2 * (width1_1)) / denominator;
+
+    // for a line segment interection, s and t must be within 0.0 and 1.0.
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+        // Collision detected
+        pointOfCollision->x = this->x1 + (t * width1);
+        pointOfCollision->y = this->y1 + (t * height1);
+        return true;
+    }
 
     return false; // No collision
 }
