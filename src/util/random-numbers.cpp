3c68a5d760b51bf41656ac6739cb393fdbae6576
Sean Ramey <sramey40@gmail.com>
2020-03-29 21:45:20 -0500

    Update some standard library #includes

--- src/util/random-numbers.cpp
+++ src/util/random-numbers.cpp
@@ -1,15 +1,15 @@
 
 #include "random-numbers.hpp"
 
-#include <time.h>
-#include <stdlib.h>
+#include <ctime>
+#include <cstdlib>
 
 	void Util::SeedRandomNumbers()
 	{
-		srand(time(0));
+		std::srand(std::time(0));
 	}
 
 	unsigned int Util::GetRandomNumber(unsigned int min, unsigned int max)
 	{
-		return (rand() % max) + min;
+		return (std::rand() % max) + min;
 	}
