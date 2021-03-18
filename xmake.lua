870a6ef5a1ed983d3d93914a26d436d0dffaeb0d
Sean Ramey <sramey40@gmail.com>
2021-03-17 23:47:54 -0500

    Updated build with proper compiler flags.

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,25 @@
 add_requires("sfml","doctest")
 
+add_rules("mode.debug", "mode.release")
+
 target("spacedungeon-lib")
 	set_kind("static")
 	set_languages("cxx17")
 	add_packages("sfml","doctest")
+
+	-- common compiler flags
+	add_cxflags("-Wpedantic", "-Wall", "-Wextra", "-Wno-deprecated", "-Wno-deprecated-declarations", "-march=native")
+	add_ldflags("-march=native")
+
+	if is_mode("debug") then
+        add_defines("DEBUG")
+		add_cxflags("-fno-omit-frame-pointer")
+		add_ldflags("-fno-omit-frame-pointer")
+    else is_mode("release")
+        add_defines("RELEASE")
+		add_cxflags("-flto")
+		add_ldflags("-flto")
+    end
 
 	-- recursively search for .cpp files under the src directory, but exclude any .cpp files under src/unit-tests directory and the main.cpp file
 	add_files("src/**.cpp|unit-tests/*.cpp|main.cpp")
@@ -26,6 +42,20 @@ target("unit-tests")
 	add_packages("sfml","doctest")
 	set_rundir("$(projectdir)")
 
+	-- common compiler flags
+	add_cxflags("-Wpedantic", "-Wall", "-Wextra", "-Wno-deprecated", "-Wno-deprecated-declarations", "-march=native")
+	add_ldflags("-march=native")
+
+	if is_mode("debug") then
+        add_defines("DEBUG")
+		add_cxflags("-fno-omit-frame-pointer")
+		add_ldflags("-fno-omit-frame-pointer")
+    else is_mode("release")
+        add_defines("RELEASE")
+		add_cxflags("-flto")
+		add_ldflags("-flto")
+    end
+
 	-- recursively search for .cpp files under the src/unit-tests directory
 	add_files("src/unit-tests/**.cpp")
 
@@ -45,6 +75,20 @@ target("spacedungeon")
 	add_deps("spacedungeon-lib")
 	add_packages("sfml","doctest")
 	set_rundir("$(projectdir)")
+
+	-- common compiler flags
+	add_cxflags("-Wpedantic", "-Wall", "-Wextra", "-Wno-deprecated", "-Wno-deprecated-declarations", "-march=native")
+	add_ldflags("-march=native")
+
+	if is_mode("debug") then
+        add_defines("DEBUG")
+		add_cxflags("-fno-omit-frame-pointer")
+		add_ldflags("-fno-omit-frame-pointer")
+    else is_mode("release")
+        add_defines("RELEASE")
+		add_cxflags("-flto")
+		add_ldflags("-flto")
+    end
 
 	-- only need the main.cpp because the rest is in the spacedungeon-lib dependency
 	add_files("src/main.cpp")
